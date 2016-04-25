570995516dc9ec524bc4c05b6aac4a73ae81e645
rajiwo <m_service@majipon.jp>
2016-04-25 23:06:01 +0900

    answer in c#

---
+++ Easy_Level/Read_More/C_Sharp/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+
+                // do something with line
+                if (line.Length > 55)
+                {
+
+                    string ws = line.Substring(0, 40);
+                    int iFind1 = ws.LastIndexOf(' ');
+                    if(iFind1 > 0)
+                    {
+                        ws = ws.Substring(0, iFind1);
+                    }
+                    ws = ws.TrimEnd();
+
+                    StringBuilder sb = new StringBuilder(ws);
+                    sb.Append("... <Read More>");
+                    Console.WriteLine(sb.ToString());
+                }
+                else
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+    }
+}
