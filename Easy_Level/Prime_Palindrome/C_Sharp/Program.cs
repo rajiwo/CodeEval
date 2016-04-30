582ab53b963fe4be48a0eb8df8c30cc2c7f2745d
rajiwo <m_service@majipon.jp>
2016-04-30 21:28:51 +0900

    answer in c#

---
+++ Easy_Level/Prime_Palindrome/C_Sharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int ans;
+        for (ans = 999; ans > 0; ans = ans - 2)
+        {
+            bool prime = true;
+            for(int j=2; j < ans; j++)
+            {
+                //割り切れるものがあれば素数ではない
+                if (ans % j == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+            if (prime == false) continue;
+
+            if (ans>99)
+            {
+                //3桁の場合は1桁目と3桁目が同じならOK
+                if (ans.ToString()[0] == ans.ToString()[2]) break;
+            } else
+            {
+                //2桁の場合は1桁目と2桁目が同じならOK
+                if (ans.ToString()[0] == ans.ToString()[1]) break;
+            }
+        }
+        Console.WriteLine(ans);
+    }
+}
