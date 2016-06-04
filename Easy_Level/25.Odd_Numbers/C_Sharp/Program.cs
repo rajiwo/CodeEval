f8af32ec2ae5b493c07680f18c9ef8fcc7be7a10
rajiwo <m_service@majipon.jp>
2016-06-04 10:19:50 +0900

    answer in c#

---
+++ Easy_Level/25.Odd_Numbers/C_Sharp/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Text;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int i=1;i<=99;i=i+2) {
+            Console.WriteLine(i);
+        }
+    }
+}
