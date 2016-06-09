2c1bc8dc34f8020b69a251fca8ac3a355395a58f
rajiwo <m_service@majipon.jp>
2016-06-09 23:40:31 +0900

    answer in c#

---
+++ Moderate_Level/2.Longest_Lines/C_Sharp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int num = 0;
+        List<string> list = new List<string>();
+
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (line == "") continue;
+
+                if (num == 0)
+                {
+                    num = Int32.Parse(line);
+                    continue;
+                }
+
+                if(list.Count == 0)
+                {
+                    list.Add(line);
+                    continue;
+                }
+
+                for(int i = 0; i<list.Count; i++)
+                {
+                    if(list[i].Length < line.Length)
+                    {
+                        list.Insert(i, line);
+                        if(list.Count > num) list.RemoveAt(num);
+                        break;
+                    }
+                }
+            }
+        foreach (string item in list)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ Moderate_Level/5.Detecting_Cycles/C_Sharp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        if (args.Length == 0)
+        {
+            string line;
+
+            //サンプルの入力を渡す
+            line = "2 0 6 3 1 6 3 1 6 3 1";
+            MyLogic(line);
+
+            line = "3 4 8 0 11 9 7 2 5 6 10 1 49 49 49 49";
+            MyLogic(line);
+
+            line = "1 2 3 1 2 3 1 2 3";
+            MyLogic(line);
+        }
+        else
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (null == line)
+                        continue;
+                    MyLogic(line);
+
+                }
+        }
+    }
+
+    static void MyLogic(string line)
+    {
+
+        //ここから下に、自分で考えたアルゴリズムを書く。
+        string[] nums = line.Split(' ');
+        int start_index;
+
+        for (start_index = nums.Length - 2; start_index >= 0; start_index--)
+        {
+            if (nums[start_index] == nums[nums.Length - 1]) break;
+        }
+        start_index++;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = start_index; i < nums.Length; i++)
+        {
+            sb.AppendFormat("{0} ", nums[i]);
+        }
+        sb.Remove(sb.Length - 1, 1);
+        Console.WriteLine(sb.ToString());
+
+    }
+}
